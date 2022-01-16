cb4cd3c5f78778f73c372566f9522f20be69a978
WenDeHor <WenDeHor>
2022-01-16 10:50:32 +0200

    3.UPDATE List toListCSV

--- src/main/java/com/example/reader/util/MineClass.java
+++ src/main/java/com/example/reader/util/MineClass.java
@@ -17,19 +17,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static java.text.MessageFormat.format;
+
 @RequiredArgsConstructor
 public class MineClass {
     public static void main(String[] args) throws IOException, ParseException {
         List<BaseReserve> baseReserveList = getBaseReserveList();
-        System.out.println(baseReserveList);
+//        System.out.println(baseReserveList);
     }
 
-    public static List<BaseReserve> getBaseReserveList()throws IOException, ParseException {
+    public static List<BaseReserve> getBaseReserveList() throws IOException, ParseException {
         JSONParser parser = new JSONParser();
         List<String> urlList = getURLList();
         List<BaseReserve> baseReserveList = new ArrayList<>();
+        List<String> toCSV = new ArrayList<>();
 
-        for (int i = 0; i < urlList.size()-1; i++) {
+        for (int i = 0; i < urlList.size() - 1; i++) {
             URL url2 = new URL(urlList.get(i));
             String stringJason = parseUrl(url2);
 
@@ -53,8 +55,15 @@ public class MineClass {
                     (String) objectList.get(0).get("id_api"),
                     Double.valueOf(String.valueOf(objectList.get(0).get("value"))));
             baseReserveList.add(model);
-        }
 
+
+        }
+        toCSV.add(getLocalDateArrayList().get(0) + "," + baseReserveList.get(0).getValue() + "," + 00.0);
+        for (int j = 1; j < baseReserveList.size(); j++) {
+            double difference = baseReserveList.get(j).getValue() - baseReserveList.get(j - 1).getValue();
+            toCSV.add(getLocalDateArrayList().get(j) + "," + baseReserveList.get(j).getValue() + "," + difference);
+        }
+        System.out.println(toCSV);
 
         return baseReserveList;
     }
@@ -80,7 +89,7 @@ public class MineClass {
         List<String> URLList = new ArrayList<>();
         List<String> dateURL = getDateURL();
         for (int i = 0; i < dateURL.size(); i++) {
-            URLList.add("https://bank.gov.ua/NBUStatService/v1/statdirectory/res?date="+dateURL.get(i)+"&json");
+            URLList.add("https://bank.gov.ua/NBUStatService/v1/statdirectory/res?date=" + dateURL.get(i) + "&json");
 //            URLList.add(format("https://bank.gov.ua/NBUStatService/v1/statdirectory/res?date={i}&json", dateURL.get(i)));
             System.out.println(URLList.get(i));
         }
@@ -88,20 +97,35 @@ public class MineClass {
     }
 
     //DATA
-    private static  LocalDate dateStart = LocalDate.of(2004, 1, 1);
+    private static LocalDate dateStart = LocalDate.of(2004, 1, 1);
     private static int numberOfMonths = (LocalDate.now().getYear() - 2004) * 12 + LocalDate.now().getMonthValue() - 1;
 
-    public  static List<String> getDateURL() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
+    public static List<LocalDate> getLocalDateArrayList(){
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
         List<LocalDate> localDateArrayList = new ArrayList<>();
-        List<String> stringList = new ArrayList<>();
+//        List<String> stringList = new ArrayList<>();
         localDateArrayList.add(dateStart);
         for (int i = 0; i < numberOfMonths; i++) {
             localDateArrayList.add(localDateArrayList.get(i).plusMonths(1));
+//            stringList.add(localDateArrayList.get(i).format(formatter));
+        }
+//        stringList.add(localDateArrayList.get(localDateArrayList.size() - 1).format(formatter));
+//        System.out.println(localDateArrayList);
+        return localDateArrayList;
+    }
+
+    public static List<String> getDateURL() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
+//        List<LocalDate> localDateArrayList = new ArrayList<>();
+        List<String> stringList = new ArrayList<>();
+        List<LocalDate> localDateArrayList = getLocalDateArrayList();
+//        localDateArrayList.add(dateStart);
+        for (int i = 0; i < numberOfMonths; i++) {
+//            localDateArrayList.add(localDateArrayList.get(i).plusMonths(1));
             stringList.add(localDateArrayList.get(i).format(formatter));
         }
         stringList.add(localDateArrayList.get(localDateArrayList.size() - 1).format(formatter));
-        System.out.println(stringList);
+//        System.out.println(localDateArrayList);
         return stringList;
     }
 }
